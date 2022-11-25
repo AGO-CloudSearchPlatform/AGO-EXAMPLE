91d91c672e6a877e7e874f56e0ba479f1159f58f
AnOldStory <hc9904@naver.com>
2022-11-26 04:39:46 +0900

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,14 +2,9 @@ import SWM from 'public/swm.png'
 
 function Sidebar() {
   return (
-    <div className="fixed top-0 h-full w-[220px] overflow-auto border-2 bg-white pt-5 text-center">
-      <img className="w-[200px] max-w-[230px] p-5" src={SWM} />
-      - 정보 공유 -
-      <br />
-      친구 위로하는 법<br />
-      1. 친구의 질문을 입력한다. <br />
-      2. 친구의 질문에 대한 답변을 확인한다. <br />
-      3. 친구와 친밀도 상승!
+    <div className="top-0 w-full h-full pt-5 overflow-auto text-center bg-gray-200">
+      <img className="inline w-[200px] max-w-[230px] p-5" src={SWM} />
+      {/* <span>대답의 좋은 예시 모음집</span> */}
     </div>
   )
 }
